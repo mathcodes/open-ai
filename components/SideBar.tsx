4d5d0eb7c29f959a968e0b8b485f688e3fd9c6e7
mathcodes <jonpchristie@gmail.com>
2023-05-01 14:45:48 -0400

    signout

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import React from 'react'
-import { signOut } from 'next-auth/react'
+import { useSession, signOut } from 'next-auth/react'
 
 import NewChat from 'components/NewChat'
 
 function SideBar() {
+  // destructure the session object
+  const { data: session } = useSession()
   return (
     <div className="flex flex-col h-screen p-2">
       <div className="flex-1">
@@ -15,12 +17,21 @@ function SideBar() {
 
           <div>{/* models */}</div>
 
-          <button onClick={() => signOut()} className="text-white">Sign Out</button>
+          {/* <button onClick={() => signOut()} className="text-white">Sign Out</button> */}
 
 
           {/* Map through the Chats */}
         </div>
       </div>
+
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image || 'https://www.jonchristie.net/favicon.png'}
+          alt=""
+          className="w-12 h-12 mx-auto mb-10 rounded-full cursor-pointer hover:opacity-50"
+        />
+      )}
     </div>
 
   )
